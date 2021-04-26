a6f98b9ef65d68502600abb70cb7b127cbbe41c9
mohdarshad1 <mdarshad181@gmail.com>
2021-04-26 16:54:18 +0530

    [Mohd Arshad] : Add UC3 e-mail Address

--- src/com/useregistration/UserRegistration.java
+++ src/com/useregistration/UserRegistration.java
@@ -10,12 +10,15 @@ public class UserRegistration {
 	{
 		System.out.println("WELCOME TO USER REGISTRATION PAGE");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name:");
+		System.out.println("Enter First Name: ");
 		String firstname = sc.nextLine();
-		System.out.println("Enter Last Name:\n ");
+		System.out.println("Enter Last Name: ");
 		String lastname = sc.nextLine();
+		System.out.println("Enter e-mail Address: ");
+		String email = sc.nextLine();
 		validateFirstName(firstname);
 		validateLastName(lastname);
+		validateEmail(email);
 	}
 	static void validateFirstName(String firstname)
 	{
@@ -28,13 +31,23 @@ public class UserRegistration {
 			System.out.println("Invalid First Name");
 	}
 	static void validateLastName(String lastname)
-   {
-      String regexPattern = "^[A-Z][a-z]{2,}$";
-      Pattern p = Pattern.compile(regexPattern);
-      Matcher m = p.matcher(lastname);
-      if(m.matches())
-         System.out.println("Valid Last Name");
-      else
-         System.out.println("Invalid Last Name");
-   }
+	{
+		String regexPattern = "^[A-Z][a-z]{2,}$";
+		Pattern p = Pattern.compile(regexPattern);
+		Matcher m = p.matcher(lastname);
+		if(m.matches())
+			System.out.println("Valid Last Name");
+		else
+			System.out.println("Invalid Last Name");
+	}
+	static void validateEmail(String email)
+	{
+		String regexPattern = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+		Pattern p = Pattern.compile(regexPattern);
+		Matcher m = p.matcher(email);
+		if(m.matches())
+			System.out.println("Valid e-mail Address");
+		else
+			System.out.println("Invalid e-mail Address");
+	}
 }
